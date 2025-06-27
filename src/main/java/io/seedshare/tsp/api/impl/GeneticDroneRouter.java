4a1f9ccd9deda3daddd9694f646434d73b32d339
dan <dan@debian.attlocal.net>
2025-06-26 19:30:47 -0700

    added configuration for jmh and test benchmark

--- src/main/java/io/seedshare/tsp/api/impl/GeneticDroneRouter.java
+++ src/main/java/io/seedshare/tsp/api/impl/GeneticDroneRouter.java
@@ -21,9 +21,7 @@ import io.seedshare.tsp.model.ServiceDestination;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 /** Routes the drone's path using genetic approximation */
---
+++ src/test/java/io/seedshare/tsp/api/impl/BenchmarkTest.java
@@ -0,0 +1,10 @@
+package io.seedshare.tsp.api.impl;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+public class BenchmarkTest {
+  @Benchmark
+  public void init() {
+    // Do nothing
+  }
+}
